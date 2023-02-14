e48f8c6606c3cf83f2b0e0e40524492a02b562da
Thangril <50687122+Thangril@users.noreply.github.com>
2023-02-15 08:46:27 +0900

    Updated to Typescript

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,8 +1,13 @@
 import Card from "../card/card.component";
+import { Monster } from "../../App";
 import './card-list.style.css';
 
+type CardListProps = {
+    monsters: Monster[];
+}
+
 // Destructering inside the parameter
-const CardList = ({ monsters}) => ( 
+const CardList = ({ monsters}: CardListProps) => ( 
     <div className="card-list">
         {monsters.map(monster => {                        
             return <Card monster={monster}></Card>       
